19f7515968c47cf2f209471a8851624d0d53c122
ihong sinopac <ihongChen@bank.sinopac.com>
2018-01-05 17:40:24 +0800

    monthly report finished

--- price_schema.sql
+++ price_schema.sql
@@ -13,6 +13,25 @@ create table twstock_daily_price(
 	CONSTRAINT pk_stock_price PRIMARY KEY(證券代號,yyyymmdd)
 )
 
-select count(*) from twstock_daily_price
 
-drop table twstock_daily_price
+create table twstock_monthly_revenue(
+	公司代號 varchar(10),
+	yyyymmdd smalldatetime,
+	公司名稱 varchar(20),
+	當月營收 decimal(15,1),
+	上月營收 decimal(15,1),
+	[上月比較增減(%)] decimal(15,2),
+	去年當月營收 decimal(15,1),
+	[去年同月增減(%)] decimal(15,2),
+	當月累計營收 decimal(15,1),
+	去年累計營收 decimal(15,1),
+	[前期比較增減(%)] decimal(15,2),
+	[上市櫃] nvarchar(10),
+	CONSTRAINT pk_stock_revenue PRIMARY KEY(公司代號,yyyymmdd)
+)
+select * from twstock_monthly_revenue
+where 公司名稱 
+-- select count(*) from twstock_daily_price
+
+drop table twstock_monthly_revenue
+
